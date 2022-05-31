a232f0026355eaa45b3981b20660f05b13d43b0a
priscilla ma <priscillama621@gmail.com>
2022-05-31 15:16:48 -0400

    page should be up, but have to reroute for imgs.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-// const express = require('express');
-// const path = require('path');
+const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
-// const app = express();
+const app = express();
 
-// app.get('/', (req, res) =>
-//     res.sendFile(path.join(__dirname, 'index.httml'))
-// );
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, 'index.html'))
+);
 
-// app.listen(PORT, () =>
-//     console.log(`listening at http://localhost:${PORT}`)
-// );
+app.listen(PORT, () =>
+    console.log(`click me http://localhost:${PORT}`)
+);
