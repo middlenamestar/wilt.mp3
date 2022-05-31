b04c7691460f1155931cf0838b14bc09b7a6238a
priscilla ma <priscillama621@gmail.com>
2022-05-31 15:10:14 -0400

    capitalized port

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-const port = process.env.PORT || 3001
+// const express = require('express');
+// const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+// const app = express();
+
+// app.get('/', (req, res) =>
+//     res.sendFile(path.join(__dirname, 'index.httml'))
+// );
+
+// app.listen(PORT, () =>
+//     console.log(`listening at http://localhost:${PORT}`)
+// );
