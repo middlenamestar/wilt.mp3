5b7f980ceceeec0f88ab630b32013f07c0ae4104
priscilla ma <priscillama621@gmail.com>
2022-06-18 01:55:52 -0400

    connected mongoose to atlas, added login page route, created session middleware

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
+
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/wilt',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    () => {
+        console.log('connected')
+    }
+);
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(session({
+    secret: 'meow 5555',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}));
 
 // serve static files
 app.use(express.static('public'));
@@ -11,6 +32,11 @@ app.use(express.static('public'));
 // don't think i have to do /public, but gonna leave it for now
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// have to do public for this one.
+app.get('/user-login', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/user-login.html'))
 );
 
 app.listen(PORT, () =>
