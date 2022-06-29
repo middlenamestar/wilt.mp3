b939c365d8a1f8fee0d6758ce395d10ea5cfe0d0
priscilla ma <priscillama621@gmail.com>
2022-06-28 22:59:04 -0400

    created login route

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('./models/User');
 const generateToken = require('./auth');
+const bcrypt = require('bcrypt');
 
 // create new user
 router.post('/users', async (req, res) => {
@@ -9,7 +10,9 @@ router.post('/users', async (req, res) => {
             username: req.body.username,
             password: req.body.password
         })
+        // save to db
         await newUser.save()
+
         const token = generateToken(newUser._id)
 
         console.log(`new user: ${newUser} and token: ${token}`);
@@ -19,6 +22,29 @@ router.post('/users', async (req, res) => {
     }
 });
 
-module.exports = router;
-
 // add a "if user already exists" error to make sure duplicate users are not registered.
+
+// login
+router.post('/users/login', async (req, res) => {
+    try {
+        const {username, password} = req.body
+
+        // search db
+        const user = await User.findOne({username})
+
+        if(user && (await bcrypt.compare(password, user.password))) {
+            // success. logged in.
+            const token = generateToken(user._id)
+            console.log(`successfully logged in, here is jwt token: ${token}`)
+            res.send('u r now logged in ☆')
+        } else {
+            // please check your credentials!
+            console.log('invalid credentials')
+            res.send('nah')
+        }
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+module.exports = router;
