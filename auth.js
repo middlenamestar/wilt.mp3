4f5af01af7b9630ca3d088b6767f13b41e097739
priscilla ma <priscillama621@gmail.com>
2022-08-24 13:04:58 -0400

    jwt auth helper

--- auth.js
+++ auth.js
@@ -1,9 +1,44 @@
 const jwt = require('jsonwebtoken');
+const User = require('./models/User');
 
-const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
+// generates jwt token
+const generateToken = (username) => {
+    return jwt.sign({ username }, process.env.JWT_SECRET, {
         expiresIn: '30d'
     })
 };
 
-module.exports = generateToken;
+// protect route
+const protect = async (req, res, next) => {
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            // get token from header
+            token = req.headers.authorization.split(' ')[1];
+
+            // verify token
+            const decoded = jwt.verify(token, proess.env.JWT_SECRET);
+
+            // get user from token
+            req.user = await User.findById(decoded.id).select('-password');
+            // console.log(decoded);
+            // console.log(decoded.id);
+
+            next();
+        } catch (error) {
+            // not the right token
+            console.log(error);
+            res.status(401);
+            console.log('not authorized')
+        }
+    };
+
+    if(!token) {
+        // non existant token
+        console.log('not authorized, no token');
+        res.send('not authorized, no token');
+    };
+};
+
+module.exports = { generateToken, protect };
